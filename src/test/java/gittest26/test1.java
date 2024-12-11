2511503ef2679e4082c2a2a32a66b48182e332f8
tester <ashwini.rao2105@gmail.com>
2024-12-11 17:37:42 +0530

    first master push

---
+++ src/test/java/gittest26/test1.java
@@ -0,0 +1,10 @@
+package gittest26;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("master m1 first commit");
+	}
+
+}
