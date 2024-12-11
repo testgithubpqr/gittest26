ab2bd3e7ab3ea24ed1df3faa849c2ba0dbeee0da
tester <ashwini.rao2105@gmail.com>
2024-12-11 17:56:54 +0530

    m2 commit

--- src/test/java/gittest26/test1.java
+++ src/test/java/gittest26/test1.java
@@ -6,6 +6,7 @@ public class test1 {
 		// TODO Auto-generated method stub
 		System.out.print("master m1 first commit");
 		System.out.print("another m1 first commit");
+		System.out.print("master m1 second commit");
 	}
 
 }
