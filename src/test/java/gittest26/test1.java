d7159cfe9fed1942e536dc3166a68d6f0aeee4fc
Admin <Admin@DESKTOP-QOJI5AF.localdomain>
2024-12-11 17:48:02 +0530

    another first commit

--- src/test/java/gittest26/test1.java
+++ src/test/java/gittest26/test1.java
@@ -5,6 +5,7 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("master m1 first commit");
+		System.out.print("another m1 first commit");
 	}
 
 }
